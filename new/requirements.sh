53e76986899001a4a1a97dcaf0c07871775374a2
BrunooCastro <bruno.scastro2012@hotmail.com>
2020-05-12 20:05:07 -0300

    Requirements running now

--- new/requirements.sh
+++ new/requirements.sh
@@ -1,7 +1,5 @@
-sudo su
-apt update
-apt upgrade
-apt install -y python3-dev
+sudo apt update && apt upgrade
+sudo apt install -y python3-dev
 pip3 install numpy
 pip3 install pandas
 pip3 install tqdm
