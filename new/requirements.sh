dbdf4e7e79bc4a1dbc1cdf1820b52939055d77a3
Bruno Castro <38269840+brunoocastro@users.noreply.github.com>
2020-05-12 20:22:21 -0300

    Update requirements.sh

--- new/requirements.sh
+++ new/requirements.sh
@@ -2,9 +2,9 @@ sudo apt update && apt upgrade
 sudo apt install -y python3-dev
 pip3 install numpy
 pip3 install pandas
+pip3 install matplotlib
+pip3 install wheel
 pip3 install tqdm
 pip3 install vispy
 pip3 install modin
 pip3 install modin[ray]
-pip3 install matplotlib
-pip3 install wheel
