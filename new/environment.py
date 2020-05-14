c3a07003e34611454d90a593b646ea35bf2bbcea
mggmauricio <mauriciomgodoy2@hotmail.com>
2020-05-14 18:27:04 -0300

    Get_observantion e Step atualizadas

--- new/environment.py
+++ new/environment.py
@@ -11,12 +11,22 @@ import modin.pandas as pdm
 
 #-------------------------------------------------FUNCOES TOP-----------------------------------------------------------
 
+def ang_tg(pg,pb):
+    
+    xgp = pg[0] - pb[0]
+    ygp = pg[1] - pb[1]
+    xgp = pg[2] - pb[2]
+    
+    axy = np.arctg(ygp/xgp)
+    azx = np.arctg(zgp/xgp)
+    return axy, azx
 
-def deg2rad(deg):
-	"""Convert angles from degrees to radians.
-	"""
-	return np.pi * deg / 180.0
-
+def angulos(v1, v2):
+	d = [abs(v1[i] - v2[i]) for i in range(3)]
+	h_l = math.sqrt(d[0]**2 + d[1]**2)
+	r = math.degrees(math.atan2(d[0],d[1]))
+	theta = math.degrees(math.atan2(h_l,d[2]))
+	return r, theta
 
 def dh(a, alfa, d, theta):
 	"""Builds the Homogeneous Transformation matrix	corresponding to each line of the Denavit-Hartenberg parameters.
@@ -29,12 +39,11 @@ def dh(a, alfa, d, theta):
 	])
 	return m
 
-
 def fk(mode, goals):
 	"""Forward Kinematics.
 	"""
 	# Convert angles from degrees to radians
-	t = [deg2rad(x) for x in goals]
+	t = [math.radians(x) for x in goals]
 	# Register the DH parameters
 	hs = [dh(0, -np.pi / 2, 4.3, t[0])]
 	if mode >= 2:
@@ -86,13 +95,27 @@ class Environment:
 	def get_observations(self):
 		distances = np.array([])
 		for p in range(self.obj_number):
+			# Computing all distances between the terminal and the objective points.
 			mod_dist = np.array(
 				[(abs(self.joints_coordinates[3, i] - self.points[p, i])) for i in range(3)])  # old self.points
 			euc_dist = np.array(math.sqrt(math.sqrt(mod_dist[:, 0] ** 2 + mod_dist[:, 1] ** 2) ** 2 + mod_dist[:, 2] ** 2))
 			distances = np.vstack((distances, euc_dist))
-		return distances
+
+		v1 = self.joints_coordinates[3,:]
+		for i in range(self.obj_number):	
+			v2 = self.points[p, :]
+			n1 = math.sqrt(x1**2 + y1**2 + z1**2) #norma do ponto 1
+			n2 = math.sqrt(x2**2 + y2**2+ z2**2) #norma ponto 2
+			norma = n1 * n2 
+			internal_product = np.dot(v1,v2)
+			angle = 1/(internal_product/norma)
+		
+		
+		obs = 0
+		return obs
 
 	def is_done(self):
+		# Check and validate if the epoch is done.
 		done = False
 		for p in range(self.obj_number):
 			validation_test = []
@@ -114,7 +137,7 @@ class Environment:
 			done = self.steps_left == 0
 		return done
 
-	def action(self, action):
+	def action(self, action, obs):
 		negative_reward = False
 
 		# Generating route to manipulator plot
@@ -131,15 +154,14 @@ class Environment:
 				negative_reward = True
 			time.sleep(0.005)
 
+		obs2 = self.get_observations()
+
 		if self.is_done():
 			raise Exception("Game is Over")
 
 		self.steps_left -= 1
-		if negative_reward:
-			reward = -1
-		else:
-			reward = np.random.random()
-		return reward
+		reward = self.get_reward(obs, obs2, negative_reward)
+		return reward, obs2
 
 	def action_sample(self):
 		sample = [np.random.randint(low=-180, high=180, size=1)[0] for i in range(4)]
@@ -168,9 +190,16 @@ class Environment:
 		obs = self.get_observations()
 		return obs
 
+	def get_reward(self, obs, obs2, negative_reward):
+		min_dist = min(obs[:self.obj_number])
+		min_dist2 = min(obs2[:self.obj_number])
+		reward = np.tanh(min_dist-min_dist2)
+		if negative_reward:
+			reward = -1
+		return reward
+	
 	def render(self):
 		pass
-
 
 class Agent:
 	"""This class control the data flow of the agent using the forward kinematics	functions. If you want to create a
@@ -183,8 +212,7 @@ class Agent:
 
 	def step(self, action):
 		obs = self.env.get_observations()
-		reward = self.env.action(action)
-		obs2 = self.env.get_observations()
+		reward, obs2 = self.env.action(action, obs)
 		done = self.env.is_done()
 		self.total_reward += reward
 		return obs, obs2, reward, done
@@ -192,114 +220,36 @@ class Agent:
 
 # ------------------------------------------------------CÓDIGO VELHO---------------------------------------------------
 
-fig = plt.figure()
+'''
+def animate(self, i):
+	x, y, z = [np.array(i) for i in [self.df.x, self.df.y, self.df.z]]
+	self.ax.clear()
+	self.ax.plot3D(x, y, z, 'gray', label='Links', linewidth=5)
+	self.ax.scatter3D(x, y, z, color='black', label='Joints')
+	self.ax.scatter3D(x[3], y[3], zs=0, zdir='z', label='Projection', color='red')
+	# self.ax.scatter3D(0, 0, 4.3, plotnonfinite=False, s=135000, norm=1, alpha=0.2, lw=0)
+	x, y, z = [np.array(i) for i in [self.trajectory.x, self.trajectory.y, self.trajectory.z]]
+	self.ax.plot3D(x, y, z, c='b', label='Trajectory')
 
+	if self.plotpoints == True:
+		x, y, z, = [], [], []
+		n_x, n_y, n_z = [np.array(i) for i in [self.points.x, self.points.y, self.points.z]]
+		for i in range(self.obj_number[0]):
+			if self.boolplot[i]:
+				x.append(n_x[i])
+				y.append(n_y[i])
+				z.append(n_z[i])
 
-def showPlot(arm):
-	ani = FuncAnimation(fig, arm.animate, interval=0.005)
-	plt.show()
+		legend = 'Objectives: ' + str(self.obj_remaining[0]) + '/' + str(self.obj_number[0])
+		self.ax.scatter3D(x, y, z, color='green', label=legend)
 
-
-class arm(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self)
-		self.zeros = np.array([210.0, 180.0, 65.0, 153.0])
-		self.goals = np.array([0.0 for i in range(4)])
-		self.plotpoints = False
-		self.trajectory = pd.DataFrame({'x': [], 'y': [], 'z': []})
-		self.obj_number = 0
-		self.obj_remaining = 0
-		self.ax = plt.gca(projection='3d')
-		self.reset = False
-		self.done = False
-		self.n_obs = [0, 0, 0, 0]
-		self.actual_epoch = 0
-		self.actual_step = 0
-		self.old_fixed_reward = 0
-
-	def run(self):
-		## Isso cuida da trajetoria
-		rt = threading.Thread(name='realtime', target=self.realtime)
-		rt.setDaemon(True)
-		rt.start()
-		time.sleep(1.0)
-
-		## Isso cuida dos objetivos
-		obj = threading.Thread(name='objectives', target=self.objectives)
-		obj.setDaemon(True)
-		obj.start()
-		time.sleep(1.0)
-
-		## Isso cuida das observações, vulgo pontos no espaço
-		dis = threading.Thread(name='distance', target=self.distanced)
-		dis.setDaemon(True)
-		dis.start()
-		time.sleep(1.0)
-
-	def get_reward(self):
-		fixed_reward = (self.obj_number - self.obj_remaining) / self.obj_number - self.old_fixed_reward
-		threshold = 100
-		touch_ground = 0
-		if self.negative_reward:
-			touch_ground = -200
-			print("Touch the ground!!!")
-		weight = self.obj_number / threshold
-		variable_reward = []
-		for i in range(int(self.obj_number)):
-			if ((self.old_distance[i] <= threshold) and self.old_distance[i] != 0):
-				variable_reward.append(weight / self.old_distance[i])
-		old_variable_reward = sum(variable_reward)
-		variable_reward = []
-		for i in range(int(self.obj_number)):
-			if ((self.distanced()[i] <= threshold) and self.distanced()[i] != 0):
-				variable_reward.append(weight / self.distanced()[i])
-		variable_reward = sum(variable_reward)
-		reward = ((fixed_reward * 2 + (variable_reward - old_variable_reward)) * 100) + touch_ground
-		self.old_fixed_reward = fixed_reward
-		if self.negative_reward:
-			_ = self.resety()
-		return reward
-
-	def clear_trajectory(self):
-		self.trajectory = pd.DataFrame({'x': [], 'y': [], 'z': []})
-
-	def resety(self):
-		self.clear_trajectory()
-		self.reset = True
-		self.plotpoints = False
-		self.old_fixed_reward = 0
-		self.negative_reward = False
-		self.goals = np.array([0.0 for i in range(4)])
-		return self.distanced()
-
-	def animate(self, i):
-		x, y, z = [np.array(i) for i in [self.df.x, self.df.y, self.df.z]]
-		self.ax.clear()
-		self.ax.plot3D(x, y, z, 'gray', label='Links', linewidth=5)
-		self.ax.scatter3D(x, y, z, color='black', label='Joints')
-		self.ax.scatter3D(x[3], y[3], zs=0, zdir='z', label='Projection', color='red')
-		# self.ax.scatter3D(0, 0, 4.3, plotnonfinite=False, s=135000, norm=1, alpha=0.2, lw=0)
-		x, y, z = [np.array(i) for i in [self.trajectory.x, self.trajectory.y, self.trajectory.z]]
-		self.ax.plot3D(x, y, z, c='b', label='Trajectory')
-
-		if self.plotpoints == True:
-			x, y, z, = [], [], []
-			n_x, n_y, n_z = [np.array(i) for i in [self.points.x, self.points.y, self.points.z]]
-			for i in range(self.obj_number[0]):
-				if self.boolplot[i]:
-					x.append(n_x[i])
-					y.append(n_y[i])
-					z.append(n_z[i])
-
-			legend = 'Objectives: ' + str(self.obj_remaining[0]) + '/' + str(self.obj_number[0])
-			self.ax.scatter3D(x, y, z, color='green', label=legend)
-
-		title = 'Epoch: ' + str(self.actual_epoch) + ' Step: ' + str(self.actual_step)
-		self.ax.set_title(title, size=10)
-		self.ax.legend(loc=2, prop={'size': 7})
-		self.ax.set_xlabel('x')
-		self.ax.set_ylabel('y')
-		self.ax.set_zlabel('z')
-		self.ax.set_xlim([-60, 60])
-		self.ax.set_ylim([-60, 60])
-		self.ax.set_zlim([0, 60])
+	title = 'Epoch: ' + str(self.actual_epoch) + ' Step: ' + str(self.actual_step)
+	self.ax.set_title(title, size=10)
+	self.ax.legend(loc=2, prop={'size': 7})
+	self.ax.set_xlabel('x')
+	self.ax.set_ylabel('y')
+	self.ax.set_zlabel('z')
+	self.ax.set_xlim([-60, 60])
+	self.ax.set_ylim([-60, 60])
+	self.ax.set_zlim([0, 60])
+'''
